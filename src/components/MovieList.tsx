0f779cbd01d7c8e11a77d860836b8815df0342ff
Kwonyeojiny <kangagi0009@naver.com>
2024-11-07 03:51:28 +0900

    더미데이터를 사용한 메인 페이지(App.tsx) 레이아웃 구성

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import movieDatas from '../data/movieListData.json';
+import MovieCard from './MovieCard';
+
+const MovieList: React.FC = () => {
+  return (
+    <div className="flex flex-wrap gap-4 justify-center items-center">
+      {movieDatas.results.map(movie => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          vote_average={movie.vote_average}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
