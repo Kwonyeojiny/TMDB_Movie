1aecc5070167d975b939b8d4fdbb6d0eaab12526
Kwonyeojiny <kangagi0009@naver.com>
2024-11-07 10:31:59 +0900

    Refactor: MoveDetail 구조분해 할당

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -4,17 +4,12 @@ import movieDetailData from '../data/movieDetailData.json';
 const baseUrl = 'https://image.tmdb.org/t/p/w500';
 
 const MovieDetail = () => {
-  const [movieData, setMovieData] = useState<any>(null);
+  const [movieData, setMovieData] = useState<any>({});
+
+  const { poster_path, title, vote_average, genres, overview } = movieData && movieData;
 
   useEffect(() => {
-    const DetailData = {
-      poster_path: movieDetailData.belongs_to_collection.poster_path,
-      title: movieDetailData.title,
-      vote_average: movieDetailData.vote_average,
-      genres: movieDetailData.genres,
-      overview: movieDetailData.overview,
-    };
-    setMovieData(DetailData);
+    setMovieData(movieDetailData);
   }, []);
 
   if (!movieData) {
@@ -25,24 +20,17 @@ const MovieDetail = () => {
     <div className="flex justify-center items-center m-8 rounded-lg overflow-hidden shadow-xl transition-all duration-300">
       <article className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4">
         <div className="flex justify-center items-center">
-          <img
-            src={`${baseUrl}${movieData.poster_path}`}
-            className="object-cover m-4 rounded-lg w-full "
-          />
+          <img src={`${baseUrl}${poster_path}`} className="object-cover m-4 rounded-lg w-full " />
         </div>
         <section className="flex flex-col m-4">
           <div className="flex mt-4 mb-4">
-            <h1 className="flex-grow text-left font-bold text-4xl">{movieData.title}</h1>
-            <p className="flex justify-center items-center text-xl mr-8">
-              {movieData.vote_average}
-            </p>
+            <h1 className="flex-grow text-left font-bold text-4xl">{title}</h1>
+            <p className="flex justify-center items-center text-xl mr-8">{vote_average}</p>
           </div>
           <ul className="m-4 flex gap-4 list-none justify-center font-bold">
-            {movieData.genres.map((genre: any, index: number) => (
-              <li key={index}>{genre.name}</li>
-            ))}
+            {genres && genres.map((genre: any, index: number) => <li key={index}>{genre.name}</li>)}
           </ul>
-          <div className="mt-4 text-base">{movieData.overview}</div>
+          <div className="mt-4 text-base">{overview}</div>
         </section>
       </article>
     </div>
