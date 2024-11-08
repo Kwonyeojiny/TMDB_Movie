441ef022ee83904fa069e768eecb78b25099121a
Kwonyeojiny <kangagi0009@naver.com>
2024-11-08 14:41:34 +0900

    Design: Detail 컴포넌트Style 수정

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -31,22 +31,37 @@ const MovieDetail = () => {
   }
 
   return (
-    <div className="flex justify-center items-center m-8 rounded-lg overflow-hidden shadow-xl transition-all duration-300">
-      <article className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-4">
-        <div className="flex justify-center items-center">
-          <img src={`${baseUrl}${poster_path}`} className="object-cover m-4 rounded-lg w-full " />
-        </div>
-        <section className="flex flex-col m-4">
-          <div className="flex mt-4 mb-4">
-            <h1 className="flex-grow text-left font-bold text-4xl">{title}</h1>
-            <p className="flex justify-center items-center text-xl mr-8">{vote_average}</p>
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center m-8 mt-24 w-[800px] rounded-lg overflow-hidden shadow-xl transition-all duration-300">
+        <article className="grid grid-cols-1 md:grid-cols-2  gap-8 md:gap-4">
+          <div className="flex justify-center items-center">
+            <img
+              src={`${baseUrl}${poster_path}`}
+              className="object-cover m-4 rounded-lg w-full h-full"
+            />
           </div>
-          <ul className="m-4 flex gap-4 list-none justify-center font-bold">
-            {genres && genres.map((genre: any, index: number) => <li key={index}>{genre.name}</li>)}
-          </ul>
-          <div className="mt-4 text-base">{overview}</div>
-        </section>
-      </article>
+          <section className="flex flex-col m-4">
+            <div className="flex mt-4 mb-4">
+              <h1 className="flex-grow text-left font-bold text-4xl">{title}</h1>
+              <p className="flex justify-center items-center text-l mr-4">평점: {vote_average}</p>
+            </div>
+            <hr />
+            <ul className="m-4 flex gap-4 list-none justify-center ">
+              {genres &&
+                genres.map((genre: any, index: number) => (
+                  <li
+                    key={index}
+                    className="bg-gray-200 text-xs text-gray-800 px-4 py-1 rounded-full border border-gray-300 shadow-sm"
+                  >
+                    {genre.name}
+                  </li>
+                ))}
+            </ul>
+            <hr />
+            <div className="mt-4 text-base">{overview}</div>
+          </section>
+        </article>
+      </div>
     </div>
   );
 };
