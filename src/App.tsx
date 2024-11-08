1c9b905c350381db4aab0cc03501c6dc519680c4
Kwonyeojiny <kangagi0009@naver.com>
2024-11-08 11:47:46 +0900

    Fix: 라우팅 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetail from './pages/MovieDetail';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MovieList />} />
-        <Route path="/details" element={<MovieDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MovieList />} />
+      <Route path="/details:id" element={<MovieDetail />} />
+    </Routes>
   );
 }
 
